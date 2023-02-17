72aba6532c9affa79a4b1d962d5e2faccc83e4c5
tiyegordon <tiyea.gordon@gmail.com>
2023-02-17 18:34:47 +0000

    puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#index <studio.h>
+
+/**
+ * *main Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print 9"with proper grammar, but the outcocme is a piece of art, \n");
+	return (0);
+}
+
