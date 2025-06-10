b7be5bcbe5a9254e2ab0ea7abf5174c261a3464a
tiyegordon <tiye.gordon@gmail.com>
2025-06-10 19:19:53 -0400

    betty formatting

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints the size of various types on the computer it is compiled and run on.
+ * main - prints the size of various types
  * Return: always 0
  */
 int main(void)
@@ -8,7 +8,7 @@ int main(void)
 printf("Size of char: %zu byte(s),\n", sizeof(char));
 printf("Size of int: %zu byte(s),\n", sizeof(int));
 printf("Size of long int: %zu byte(s),\n", sizeof(long));
-printf("Size of long long: %zu byte(s),\n", sizeof(long long));
+printf("Size of long long: %zu byte(s),\n", sizeof(long long))
 printf("Size of float: %zu byte(s),\n", sizeof(float));
 return (0);
 }
