5bb2f6c730f25ad21815bf6b461500f3c75bc8e1
tiyegordon <tiye.gordon@gmail.com>
2025-06-10 19:21:12 -0400

    syntax

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 printf("Size of char: %zu byte(s),\n", sizeof(char));
 printf("Size of int: %zu byte(s),\n", sizeof(int));
 printf("Size of long int: %zu byte(s),\n", sizeof(long));
-printf("Size of long long: %zu byte(s),\n", sizeof(long long))
+printf("Size of long long: %zu byte(s),\n", sizeof(long long));
 printf("Size of float: %zu byte(s),\n", sizeof(float));
 return (0);
 }
