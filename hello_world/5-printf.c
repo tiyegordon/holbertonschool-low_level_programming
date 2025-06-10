e3b8b5071a891790531d68a6a3e3380fda184c22
tiyegordon <tiye.gordon@gmail.com>
2025-06-10 17:46:27 -0400

    updated

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints a message following a new line.
+ * Return: always 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
