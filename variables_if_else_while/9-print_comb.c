fb71f99f3ba8044457ff7e7ff0c4d2c8c9fee399
tiyegordon <tiye.gordon@gmail.com>
2025-06-12 17:34:59 -0400

    test

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints a message following a new line.
+ * Return: always 0
+ */
+int main(void)
+{
+int n;
+for (n = 0; numbers <= 9; n++)
+}
+putchar(n + '0'); /**change to a number*/
+if (n != 9) /** n is not the last number*/
+{
+putchar(','); /** print the comma*/
+putchar(' '); /** print the space*/
+}
+putchar('\n');
+return (0);
+}
