5f0521b020906ac25a46aae3c27a6e16ad9a897b
tiyegordon <tiye.gordon@gmail.com>
2025-06-13 13:06:25 -0400

    test

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -11,18 +11,13 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c) /** _putchar - writes the character c to stdout*/
-{
-return (write(1, &c, 1));
-}
-
-int main (void)
+void print_alphabet(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++);
 _putchar(letter);
 _putchar ('\n');
 
-return (0);
+return;
 
 }
