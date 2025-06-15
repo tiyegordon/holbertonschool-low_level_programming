6ef8a7b158f25ac3b68626ca294b91d8db05db9e
tiyegordon <tiye.gordon@gmail.com>
2025-06-15 00:31:36 -0400

    updated betty formating and documentation

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,23 +3,11 @@
 #include <stdio.h>
 
 /**
- * main - prints a message following a new line.
- * Return: always 0
- *
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-/**
- *_islower - Shows 1 if the input is a lowercase character. 
- *Another cases, shows 0.
- *@c: The character is ASCII code1 for lowercase character.
- * Return: 1 for lowercase character. 0 for the rest.
- */
-
+* _islower - Checks if a character is lowercase.
+* @c: The character to check (ASCII code)
+*
+* Return: 1 if c is lowercase, 0 otherwise
+*/
 
 int _islower(int c)
 {
