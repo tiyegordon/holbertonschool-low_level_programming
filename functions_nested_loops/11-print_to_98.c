5f5161c26729ea3c38d2ce0113213bb52124cff0
tiyegordon <tiye.gordon@gmail.com>
2025-06-14 23:54:25 -0400

    bettyupdated

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -4,11 +4,13 @@
 
 /**
   * Prints all natural numbers from n to 98, followed by a new line.
+  *@n:The starting integer
   *
-  * Returns 0
+  * Description: If n> 98, counts down. If n< 98, counts up. If n == 98, prints.
+  * Returns integer
   */
 
-void print_to_98(int n) /* Prints all natural numbers from n to 98*/
+void print_to_98(int n) /* Prints all natural numbers from n to 98/
 {
 if (n >= 98)
 {
