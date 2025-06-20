499f346e312d3ad4c75a1a69a6dfcd8bd1d2520c
tiyegordon <tiye.gordon@gmail.com>
2025-06-20 13:22:07 -0400

    formatting update

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -13,7 +13,7 @@ void print_most_numbers(void) /** function doesnt take any args, returns nothing
 int n; /* Declare*/
 for (n = 0; n <= 9; n++) /* start at 0; loop till 9; incrament 1 each loop */
 {
-if ( n == 2 || n == 4)
+if (n == 2 || n == 4)
 continue;
 _putchar (n + '0'); /* converting n to ASCI char, print */
 }
