852237629aac665a9ea48790d0a1e70814da8734
tiyegordon <tiye.gordon@gmail.com>
2025-06-20 13:17:54 -0400

    function that prints the numbers, from 0 to 0 followed by a new line, does not print 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* Description: print_numbers - prints the numbers from 0 to 9
+*
+* Return: Return 0
+*
+*/
+
+void print_most_numbers(void) /** function doesnt take any args, returns nothing */
+{
+int n; /* Declare*/
+for (n = 0; n <= 9; n++) /* start at 0; loop till 9; incrament 1 each loop */
+{
+if ( n == 2 || n == 4)
+continue;
+_putchar (n + '0'); /* converting n to ASCI char, print */
+}
+_putchar ('\n'); /*print a new line */
+}
