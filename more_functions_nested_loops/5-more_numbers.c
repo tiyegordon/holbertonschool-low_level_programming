d29d8af9a60c4a9583ed3991da34670d1d0a562a
tiyegordon <tiye.gordon@gmail.com>
2025-06-22 22:32:41 -0400

    updated betty

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,12 +2,12 @@
 
 /**
 * Description: more_numbers - prints the numbers from 0 to 14, 10x
-* 
+*
 * Return: Return 0
 *
 */
 
-void more_numbers(void) /** function doesn't take any arguments and returns nothing */
+void more_numbers(void)
 {
 int i, n; /* declare variables n and i */
 for (i = 0; i < 10; i++) /* outer loop*/
@@ -15,8 +15,8 @@ for (i = 0; i < 10; i++) /* outer loop*/
 for (n = 0; n <= 14; n++) /* start at 0; loop till 14; incrament 1 each loop */
 {
 if (n >= 10)
-	_putchar ((n / 10) + '0'); /* print 10s digit,converting to ASCI char, print */
-_putchar ((n % 10) + '0'); /* prints the 1s digit, converting to ASCI char, print */
+_putchar ((n / 10) + '0');
+_putchar ((n % 10) + '0');/* prints 1s digit, convert to ASCI print */
 }
 
 _putchar ('\n'); /*print a new line */
