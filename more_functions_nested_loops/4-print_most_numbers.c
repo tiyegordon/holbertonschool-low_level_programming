4c55a453a9e07589f49b7403895e010dd470b5bd
tiyegordon <tiye.gordon@gmail.com>
2025-06-22 22:29:49 -0400

    formatting updated

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,20 +2,20 @@
 #include "main.h"
 
 /**
-* Description: print_numbers - prints the numbers from 0 to 9
+* print_most_numbers - prints the numbers from 0 to 9
 *
 * Return: Return 0
 *
 */
 
-void print_most_numbers(void) /** function doesnt take any args, returns nothing */
+void print_most_numbers(void)
 {
-int n; /* Declare*/
-for (n = 0; n <= 9; n++) /* start at 0; loop till 9; incrament 1 each loop */
+int n;
+for (n = 0; n <= 9; n++)/* start at 0; loop till 9; incrament 1 each loop */
 {
 if (n == 2 || n == 4)
 continue;
 _putchar (n + '0'); /* converting n to ASCI char, print */
 }
-_putchar ('\n'); /*print a new line */
+_putchar ('\n');
 }
