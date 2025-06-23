0cb19971eaf4849528a86b47a2111ef8adc035ca
tiyegordon <tiye.gordon@gmail.com>
2025-06-22 22:34:37 -0400

    betty formating updated

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the numbers from 1 to 100, followed by a new line. 
-  * But for multiples of three print Fizz instead of the number and for the multiples of
-  * five print Buzz. 
-  * For numbers which are multiples of both three and five print FizzBuzz.  
-  * 
+  * main - Prints the numbers from 1 to 100, followed by a new line.
+  *
   * Return: Always 0 (Success)
   *
   */
