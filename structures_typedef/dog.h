0016dd59bc8be85a9f24289bfc0fc584e32bfb25
tiyegordon <tiye.gordon@gmail.com>
2025-07-06 16:29:39 -0400

    renamed struct dog to dog_t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,14 +9,19 @@
  *
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
-typedef struct dog dog_t;
+/*typedef struct dog
+{
+	char *name;
+        float age;
+        char *owner;
+} dog_t;*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
