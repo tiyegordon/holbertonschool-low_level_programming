ecac2aa43bab25b1c5bf70dcfa87cf5ec896d60c
tiyegordon <tiye.gordon@gmail.com>
2025-07-13 18:03:18 -0400

    task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/*
+ * print_name - prints a name
+ * @name: the name being printed (char)
+ * @f:
+ * Returns: Nothing
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+}
+	
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
+
